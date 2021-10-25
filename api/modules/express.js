f45fbd0761bb57a625645c945ed45e90204f8820
JustinPooters <48656828+JustinPooters@users.noreply.github.com>
2021-10-25 17:30:24 +0200

    Added Create Channel in express.js and added checks for channels
    e

--- api/modules/express.js
+++ api/modules/express.js
@@ -43,13 +43,31 @@ function expressserver() {
         }
     });
 
+    app.post('/v1/createchannel', function(req, res) {
+        let getapiheader = req.get('x-api-key');
+        if (!getapiheader || getapiheader != process.env.APIKEY) {
+            res.send(401, 'Not Authorized.');
+        } else {
+            let channelname = req.body.channelname;
+            if (!channelname) {
+                res.send(400, 'No channel name is given.');
+            } else {
+                con.query("INSERT INTO channels (channelname) VALUES ('" + channelname + "')", function(err, result, fields) {
+                    if (err) throw err;
+                    console.log(result);
+                    res.send(200, 'Channel created succesfully');
+                });
+            }
+        }
+    });
+
     app.post('/1/sendmessage', function(req, res) {
         let getapiheader = req.get('x-api-key');
         if (!getapiheader || getapiheader != process.env.APIKEY) {
             res.send(401, 'Not Authorized.');
         } else {
-            if (!req.body.message || !req.body.userid) {
-                return res.send(400, 'No message provided.');
+            if (!req.body.message || !req.body.userid || !req.body.channelid) {
+                return res.send(400, 'No message, userid or channel provided.');
             } else {
                 let userid = req.body.userid;
                 con.query("SELECT * FROM users WHERE ID=" + userid, function(err, result, fields) {
@@ -58,12 +76,22 @@ function expressserver() {
                         return res.send(400, 'No user found with that ID.');
                     } else {
                         let message = req.body.message;
-                        con.query("INSERT INTO messages (timestamp, message, author) VALUES ('" + Date.now() + "'," + message + "', " + userid + ")", function(err, result, fields) {
+                        let channelid = req.body.channelid
+                        con.query("SELECT * FROM channels WHERE ID=" + channelid, function(err, result, fields) {
                             if (err) throw err;
-                            res.send(200, 'Message sent.');
+                            if (result.length == 0) {
+                                return res.send(400, 'No channel found with that ID.');
+                            } else {
+                                con.query("INSERT INTO messages (timestamp, message, author) VALUES ('" + Date.now() + "'," + message + "', " + userid + ")", function(err, result, fields) {
+                                    if (err) throw err;
+                                    res.send(200, 'Message sent.');
+
+                                });
+                            }
                         });
                     }
                 });
+
             }
         }
     });
@@ -79,8 +107,8 @@ function expressserver() {
             } else {
                 let username = req.body.username;
                 let password = req.body.password;
+                console.log(password);
                 let encryptedpassword = cipher.update(password, 'utf8', 'hex');
-                encryptedpassword += cipher.final('hex');
                 let returnjson = `{"username": "${username}","password": "${encryptedpassword}"}`;
                 con.query("SELECT * FROM users WHERE username=" + "'" + username + "'", function(err, result, fields) {
                     if (err) throw err;
