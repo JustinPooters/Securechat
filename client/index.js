2314f07ef824b80f79d168bed9c8e9b72120996d
JustinPooters <48656828+JustinPooters@users.noreply.github.com>
2021-10-28 14:08:25 +0200

    I tried stuff with Socket.io. I failed, horribly.

--- client/index.js
+++ client/index.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
+const socketio = require('socket.io');
 
 // index page
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.listen(80);
-console.log('Server is listening on port 80');
+app.get('/chat', function(req, res) {
+    res.render('pages/chat');
+});
+
+const server = app.listen(80, () => {
+    console.log('Server started on port 80');
+});
+
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.username = "Anonymous"
+
+    socket.on('change_username', data => {
+        socket.username = data.username
+    })
+
+    //handle the new message event
+    socket.on('new_message', data => {
+        console.log("new message")
+        io.sockets.emit('receive_message', { message: data.message, username: socket.username })
+    })
+});
